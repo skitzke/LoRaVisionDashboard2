04d6c3a60e25c4244b349fe3a8b11f1804224548
Zerof8 <abdalrahman.ghname@github.com>
2021-01-13 14:21:29 +0100

    settings.blade.php last update (need fix)

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -22,99 +22,55 @@
                               </div>
 
                           @endif
-                        <button id="name-update" class="list-group-item list-group-item-action rounded-bottom " onclick="show_form('name-update')">Update Name</button>
-                           <div>
-                            <form action="{{route('update_name')}}" id="name-update-form" class="form-group card-body pt-0 pb-0 "  method="post"  hidden>
-                                @csrf
-                                <ul class="list-group">
-                                    <li class="list-group-item ">New Name<div class="col-md-6">
-                                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="{{Auth::user()->name}}" autofocus>
-                                            @error('name')
-                                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                            @enderror
-                                            <button type="submit" class="btn btn-primary">
-                                                {{ __('Apply') }}
-                                            </button>
-                                        </div></li>
-                                </ul>
-                            </form>
-                        </div>
-                          <button id="email-update" class="list-group-item list-group-item-action rounded-bottom " onclick="show_form('email-update')">Update E-mail Address</button>
-                        <div>
-                            <form action="{{route('update_email')}}" id="email-update-form" class="form-group card-body pt-0 pb-0 "  method="post"  hidden>
-                                @csrf
-                                <ul class="list-group">
-                                    <li class="list-group-item ">New Email Address<div class="col-md-6">
-                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="{{Auth::user()->email}}">
-
-                                            @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                            @enderror
-                                            <button type="submit" class="btn btn-primary">
-                                                {{ __('Apply') }}
-                                            </button>
-                                        </div></li>
-                                </ul>
-                            </form>
-                        </div>
-                        <button id="password-update" class="list-group-item list-group-item-action rounded-bottom " onclick="show_form('password-update')">Update Password</button>
-                        <div>
-                            <form action="{{route('update_password')}}" id="password-update-form" class="form-group card-body pt-0 pb-0 "  method="post"  hidden>
-                                @csrf
-                                <ul class="list-group">
-                                    <li class="list-group-item ">New Password
-                                        <div class="col-md-6">
-                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                            @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                            @enderror
-
-                                        </div>
-                                    </li>
-                                    <li class="list-group-item">
-                                    <div class="col-md-6"> Confirm New Password
-                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                    </div>
-                                    </li>
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Apply') }}
-                                    </button>
-                                </ul>
-                            </form>
-                        </div>
-
-
-                          <button data-toggle="modal" data-target="#myModal" class="list-group-item list-group-item-action rounded-bottom text-danger">
-                              Delete Account
-                          </button>
-                          {{--                        THIS IS THE POP-UP WHEN YOU PRESS ALERT LOG--}}
-                          <div class="modal fade" id="myModal" aria-hidden="true">
+                          <div>
+                              <div id="adding-form" class="form-group card-body p-0">
+                                  <ul class="list-group">
+                                      <li class="list-group-item w-100 p-0 border-0 m-2">
+                                          {{--                        THIS IS THE ADDING STATIONS BUTTON--}}
+                                          <button data-toggle="modal" data-target="#updateName" class="list-group-item list-group-item-action rounded btn btn-outline-secondary">
+                                              Update Username
+                                          </button>
+                                      </li>
+                                      <li class="list-group-item w-100 p-0 border-0 m-2">
+                                          {{--                        THIS IS THE ADDING VEHICLES BUTTON--}}
+                                          <button data-toggle="modal" data-target="#updateEmail" class="list-group-item list-group-item-action rounded btn btn-outline-secondary">
+                                              Update Email
+                                          </button>
+                                      </li>
+                                      <li class="list-group-item w-100 p-0 border-0 m-2">
+                                          {{--                        THIS IS THE ADDING SENSORS BUTTON--}}
+                                          <button data-toggle="modal" data-target="#updatePassword" class="list-group-item list-group-item-action rounded btn btn-outline-secondary">
+                                              Update Password
+                                          </button>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                          {{--                        THIS IS THE POP-UP WHEN YOU PRESS ADDING STATIONS--}}
+                          <div class="modal fade m-auto" id="updateName" aria-hidden="true">
                               <div class="modal-dialog">
                                   <div class="modal-content">
                                       <div class="modal-header">
-                                          <h4>Delete Account</h4>
+                                          <h4>Add Stations</h4>
                                           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                       </div>
                                       <div class="modal-body">
-                                          <ul>
-                                              Are you sure you want to delete your account?
-                                              <li class="list-group-item">
-                                                  <form action="{{route('delete_account', $user -> id)}}" method="post">
-                                                  @method('DELETE')
-                                                      @csrf
-                                                  <button type="submit" class="btn btn-primary">
-                                                      {{ __('Confirm Account Deletion') }}
-                                                  </button>
-                                                  </form>
-                                              </li>
-                                          </ul>
+
+
+                                          <form action="{{route('update_name')}}" id="name-update-form" class="form-group card-body pt-0 pb-0 "  method="post">
+                                              @csrf
+                                              <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="{{Auth::user()->name}}" autofocus>
+                                              @error('name')
+                                              <span class="invalid-feedback" role="alert">
+                                                       <strong>{{ $message }}</strong>
+                                                   </span>
+                                              @enderror
+                                              <button type="submit" class="btn btn-primary">
+                                                  {{ __('Apply') }}
+                                              </button>
+                                          </form>
+
+
                                       </div>
                                       <div class="modal-footer">
                                           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -122,9 +78,11 @@
                                   </div>
                               </div>
                           </div>
-                    </div>
+
+                      </div>
                 </div>
             </div>
+            {{--                        THIS IS THE AVATAR SECTION                      --}}
             <div class="col-md-3">
                 <div class="card">
                     <div class="card-header">{{ __('Update Avatar') }}</div>
